b17e7494901570daccfa99981f00f91c2cd5c571
MensurOwary <mensurowary@gmail.com>
2022-03-14 01:16:57 -0600

    Added startline and full path

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 # pipeline run ex2 CrySLDetector --datasets TSE17-ExPrecision --java-options Xmx12G --java-options Xss256m
 # pipeline publish ex2 CrySLDetector --datasets TSE17-ExPrecision -s http://localhost -u owary -p owary --java-options Xmx10G --java-options Xss128m
-# pipeline publish metadata -s http://localhost -u reviewer1 -p standalone --datasets TSE17-ExPrecision
+# pipeline publish ex2 CrySLDetector --datasets TSE17-ExPrecision -s http://localhost -u reviewer1 -p standalone --skip itext
+# pipeline publish metadata -s http://localhost -u reviewer1 -p standalone --datasets TSE17-ExPrecision --skip itext
 mvn clean package -DskipTests
 cp target/CrySLDetector.jar CrySLDetector/latest/CrySLDetector.jar
-docker run -it -v mubench-checkouts:/mubench/checkouts -v mubench-findings:/mubench/findings -v /home/owary/Programming/Research/APIMisuse/AmalgamScanner/CrySLDetector:/mubench/detectors/CrySLDetector --rm -p 8080:80 mansur/mubench:stable
+docker run -it -v mubench-checkouts:/mubench/checkouts -v mubench-findings:/mubench/findings -v /home/owary/Programming/Research/APIMisuse/CrySLScanner/CrySLDetector:/mubench/detectors/CrySLDetector --rm -p 8080:80 mansur/mubench:stable
--- src/main/java/ca/ualberta/smr/Main.java
+++ src/main/java/ca/ualberta/smr/Main.java
@@ -4,18 +4,35 @@ import de.tu_darmstadt.stg.mubench.cli.*;
 
 import static crypto.Runner.getDetectorOutput;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
         DetectionStrategy detectionStrategy = (detectorArgs, builder) -> {
-            String targetClassPath = detectorArgs.getTargetClassPaths()[0];
+            System.out.println("TARGET SRC: " + Arrays.toString(detectorArgs.getTargetSrcPaths()));
+            System.out.println("TARGET CLS: " + Arrays.toString(detectorArgs.getTargetClassPath().getPaths()));
+
+            String targetSrcPath = detectorArgs.getTargetSrcPaths()[0];
+            String targetClassPath = detectorArgs.getTargetClassPath().getClasspath();
 
             String[] newArgs = new String[]{
                     "--appPath",
                     targetClassPath
             };
 
-            return builder.withFindings(getDetectorOutput(newArgs));
+            List<DetectorFinding> detectorFindings = getDetectorOutput(newArgs).stream()
+                    .peek(df -> {
+                        String file = (String) df.get("file");
+                        df.put("file", targetSrcPath + file);
+
+                        String lineNumber = (String) df.get("line_number");
+                        df.put("startline", Integer.parseInt(lineNumber));
+                    }).collect(Collectors.toList());
+
+            return builder.withFindings(detectorFindings);
         };
 
         new MuBenchRunner()
